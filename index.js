3d037e4bce394ae19202160107a0570ebc8ee93c
TheWendril <wendril2001avila@alu.ufc.br>
2020-02-18 12:22:30 -0300

    conversão de c para js

---
+++ index.js
@@ -0,0 +1,30 @@
+
+    function quantidade_De_Parentese(text){
+        
+
+     var contador = 0;
+
+        for(item of text){
+            if(item === '('){
+                contador++;
+            }
+        }
+    }
+
+
+    function resolver_Parenteses(...params){
+        
+    }
+
+const input = document.querySelector('input');
+const buttom = document.querySelector('button');
+
+
+buttom.onclick = () => {
+
+var text = input.value;
+
+    
+      
+
+}
