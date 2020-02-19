5d728c17afdcd749818bd5a7df4ee8b01826b7f8
TheWendril <wendril2001avila@alu.ufc.br>
2020-02-18 23:28:06 -0300

    com erro no + e -

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-class realizar_Operacoes{
+var vetor_expressao = [];
 
-    constructor(){
-        var vetor = [1,2,4,5,6];
-        const result = document.querySelector('.result');
-        console.log(vetor);
+const painel = document.querySelector('.result');
+
+const botoes = document.querySelectorAll('.btn_num');
+
+const operats = document.querySelectorAll('.btn_operat'); 
+
+console.log(operats);
+console.log(botoes);
+
+function resolverPainel(){
+    
+}
+
+
+function redenrizar(){
+    painel.innerHTML = '';
+
+    for(var i = 0; i < vetor_expressao.length; i++){
+        var text = document.createTextNode(vetor_expressao[i]);
+        painel.appendChild(text);
+    }
+}
+
+
+function addPainel(valor){
+    vetor_expressao.push(valor);
+    redenrizar();
+}
+
+
+function setarAtributos(){
+
+    for(var i = 0; i < botoes.length; i++){
+
+        var valor = botoes[i].innerHTML;
+
+        botoes[i].setAttribute('onclick', 'addPainel(' + valor + ')'); 
     }
 
-    redenrizar(...params){
-        this.result.innerHTML = '';
+    for(var i = 0; i < operats.length; i++){
+        var valor = operats[i].innerHTML;
         
+
+        if(valor != '='){
+            operats[i].setAttribute('onclick', 'addPainel(' +valor+ ')');
+            }
+
+        if(valor == '='){
+            operats[i].setAttribute('onclick', 'resolverPainel()');
+            }   
     }
 
 }
 
 
-
+setarAtributos();
